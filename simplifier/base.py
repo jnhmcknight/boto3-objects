680a81efbb9ddd158d8d12b0028b7f96ebd6c63c
Jared McKnight <jmcknight@archertech.ca>
2024-06-24 11:29:29 -0400

    add in region determination, easy paginator/waiter

--- simplifier/base.py
+++ simplifier/base.py
@@ -16,6 +16,7 @@ class Boto3Base:
         self.session = session
         self.client = client
         self.tags = None
+        self._data = {}
 
     @property
     def init_args(self):
@@ -82,13 +83,70 @@ class Boto3Base:
         return self._data['Arn']
 
     @property
+    def in_africa(self):
+        return self.region_name.startswith('af-')
+
+    @property
+    def in_asia(self):
+        return self.region_name.startswith('ap-')
+
+    @property
+    def in_canada(self):
+        return self.region_name.startswith('ca-')
+
+    @property
+    def in_china(self):
+        return self.region_name.startswith('cn-')
+
+    @property
+    def in_europe(self):
+        return self.region_name.startswith('eu-')
+
+    @property
+    def in_israel(self):
+        return self.region_name.startswith('il-')
+
+    @property
+    def in_middle_east(self):
+        return self.region_name.startswith('me-')
+
+    @property
+    def in_north_america(self):
+        return self.in_canada or self._in_usa
+
+    @property
+    def in_south_america(self):
+        return self.region_name.startswith('sa-')
+
+    @property
+    def in_usa(self):
+        return self.region_name.startswith('us-')
+
+    @property
     def region_name(self):
         return self.session.region_name
+
+    @property
+    def region(self):
+        return self.region_name
 
     @property
     def account_id(self):
         from .sts import STS  # pylint: disable=import-outside-top-level
         return STS(session=self.session).account_id
+
+    def paginate(self, paginator_func, *result_keys, **kwargs):
+        paginator = self.client.get_paginator(paginator_func)
+        for page in paginator.paginate(**kwargs):
+            results = page
+            for key in result_keys:
+                results = results.get(key, [])
+            for item in results:
+                yield item
+
+    def wait(self, waiter_func, **kwargs):
+        waiter = self.client.get_waiter(waiter_func)
+        return waiter.wait(**kwargs)
 
 
 @dataclass
